0e0bb56a9c305c38252e120115ab0d22811785e5
DeltaCephei <deornorth@gmail.com>
2013-07-24 21:29:45 +1200

    Initialize repository

---
+++ vendor/bundle/gems/sqlite3-1.3.7/ext/sqlite3/siteconf20130724-3684-9200fj.rb
@@ -0,0 +1,6 @@
+require 'rbconfig'
+dest_path = "./.gem.20130724-3684-4f91l4"
+RbConfig::MAKEFILE_CONFIG['sitearchdir'] = dest_path
+RbConfig::CONFIG['sitearchdir'] = dest_path
+RbConfig::MAKEFILE_CONFIG['sitelibdir'] = dest_path
+RbConfig::CONFIG['sitelibdir'] = dest_path
